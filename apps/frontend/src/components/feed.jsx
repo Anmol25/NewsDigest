57aabd208dca4202f2dd9d6a7e0f2727d3dfd3cd
Anmol Jindal <anmoljindal025@gmail.com>
2025-02-18 21:52:21 +0530

    Improved Sidemenu CSS

--- apps/frontend/src/components/feed.jsx
+++ apps/frontend/src/components/feed.jsx
@@ -8,7 +8,7 @@ function Feed({topic}){
     
     return (
         <div className="Feed">
-            <h1>{topicfeed}</h1>
+            <h1 className="FeedTitle">{topicfeed}</h1>
             <div className="FeedList">
                 <News image="https://static.toiimg.com/photo/msid-118344813,imgsize-35574.cms" title="BBA student driving Audi crashes into scooty in Delhi; 1 critical"
                 link="link" />
--- apps/frontend/src/components/sidemenu.jsx
+++ apps/frontend/src/components/sidemenu.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "../styles/sidemenu.css";
 
 function SideMenu({changeTopic}){
-    const topics = ["For You", "Top Stories", "Latest", "India", "World", "Economy", "Science", "Tech", "Sports", "Entertainment"];
+    const [activeTopic, setActiveTopic] = useState("Top Stories");
+
+    const handleTopicClick = (topic) => {
+        setActiveTopic(topic);
+        changeTopic(topic);
+    }
+
+    const topics = ["For You", "Top Stories", "Latest", "India", "World", "Economy",
+         "Science", "Tech", "Sports", "Entertainment"];
 
     return (
         <div className="SideMenu">
             <ul>
                 {topics.map((topic, index) => {
-                    return <li key={index} onClick={() => changeTopic(topic)}>{topic}</li>
+                    return <li className={`topic-li ${activeTopic === topic ? "active" : ""}`} key={index} onClick={() => handleTopicClick(topic)}>{topic}</li>
                 })}
             </ul>
         </div>
