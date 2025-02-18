10750946e1fa5c09535124348bd8539a01140c81
Anmol Jindal <anmoljindal025@gmail.com>
2025-02-18 21:22:35 +0530

    Improved Search Bar CSS

--- apps/frontend/src/components/navbar.jsx
+++ apps/frontend/src/components/navbar.jsx
@@ -1,17 +1,17 @@
 import logo from '../assets/logo.png';
 import profile from '../assets/defaultphoto.jpg';
+import search from '../assets/search.png';
 import '../styles/navbar.css';
 
 function Navbar(){
     return (
         <div className="navbar">
             <img className='logo' src={logo} alt="News Digest"/>
-            <form className='Searchbar'>
-                <input type="text" placeholder="Search" name="search"/>
-                <button className='searchbutton' type="submit">Search</button>
+            <form className='Searchform'>
+                <input className='Searchinput' type="text" placeholder="Search" name="search"/>
+                <button className='searchbutton' type="submit"><img className='searchicon' src={search} alt="Search" /></button>
             </form>
             <img className='profile' src={profile} alt="Profile" />
-            {/* <p className='profile'>Profile</p> */}
         </div>
     )
 }
