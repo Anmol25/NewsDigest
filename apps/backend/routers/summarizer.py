f3f8e9dd4c82b8896e2a9118a63fd61cbc8b4384
Anmol Jindal <anmoljindal025@gmail.com>
2025-03-11 12:45:09 +0530

    Improved Recommendation system

--- apps/backend/routers/summarizer.py
+++ apps/backend/routers/summarizer.py
@@ -40,34 +40,3 @@ def summarize(id: int, db: Session = Depends(get_db), current_user: Users = Depe
     except Exception as e:
         raise HTTPException(status_code=404, detail={
                             "Article Summary Not Found"})
-
-
-# @router.post("/summarize")
-# def summarize(request: ArticleUrl, db: Session = Depends(get_db), current_user: Users = Depends(get_current_active_user)):
-#     try:
-#         # Retrieve url
-#         url = request.url
-#         # Check Entry of url in DB
-#         article = db.query(Articles).filter(Articles.link == url).first()
-#         if article:
-#             # Update user history:
-#             update_user_history(db, current_user.id, article.id)
-#             # Retrieve Summary if available
-#             if article.summary:
-#                 return {"data": article.summary}
-#             else:
-#                 # Generate summary and then add to DB
-#                 generated_summary = dbart.infer(url)
-#                 article.summary = generated_summary
-#                 db.commit()
-#                 # Return New generated summary
-#                 return {"data": generated_summary}
-#         # If no entry then generate summary and return summary
-#         summary = dbart.infer(url)
-#         if summary:
-#             return {"data": summary}
-#         else:
-#             return {"data": "Error in Generating Summary. Please refer to original article"}
-#     except Exception as e:
-#         logger.error(f"Error in sending summary request: {e}")
-#         raise HTTPException(status_code=500, detail=str(e))
--- apps/backend/users/recommendation.py
+++ apps/backend/users/recommendation.py
@@ -1,5 +1,5 @@
 import numpy as np
-from database.models import Articles, Users, UserHistory
+from database.models import Articles, UserHistory
 from sqlalchemy import desc
 from sqlalchemy.sql import exists
 
@@ -12,23 +12,28 @@ class Recommendar:
         return hist_exist
 
     @staticmethod
-    def get_mean_embedding(user, db, hist_len=5):
-        results = db.query(Articles.embeddings).join(
-            UserHistory, UserHistory.article_id == Articles.id).filter(UserHistory.user_id == user.id).order_by(desc(UserHistory.watched_at)).limit(hist_len).all()
-        embeddings = np.array([result[0] for result in results])
+    def get_mean_embedding_and_ids(user, db, hist_len=10):
+        results = db.query(Articles. id, Articles.embeddings).join(
+            UserHistory, UserHistory.article_id == Articles.id).filter(
+                UserHistory.user_id == user.id).order_by(desc(UserHistory.watched_at)).limit(
+                    hist_len).all()
+        if not results:
+            return None, []
+        articles_id = [result[0] for result in results]
+        embeddings = np.array([result[1] for result in results])
         mean_embedding = np.mean(embeddings, axis=0)
-        return mean_embedding
+        return mean_embedding, articles_id
 
     @staticmethod
-    def personalized_feed(mean_embedding, db, page=1, page_size=10):
+    def personalized_feed(mean_embedding, article_ids, db, page=1, page_size=10):
         offset = (page - 1) * page_size
         similar_items = []
         results = db.query(
             Articles,
             Articles.embeddings.cosine_distance(
                 mean_embedding).label('distance')
-        ).order_by('distance',
-                   Articles.published_date.desc()).offset(offset).limit(page_size).all()
+        ).filter(~Articles.id.in_(article_ids)).order_by('distance',
+                                                         Articles.published_date.desc()).offset(offset).limit(page_size).all()
 
         for item, score in results:
             result_item = {
@@ -50,7 +55,8 @@ class Recommendar:
         hist_exist = Recommendar.check_history(user, db)
         if not hist_exist:
             return []
-        mean_embedding = Recommendar.get_mean_embedding(user, db)
+        mean_embedding, article_ids = Recommendar.get_mean_embedding_and_ids(
+            user, db)
         feed = Recommendar.personalized_feed(
-            mean_embedding, db, page, page_size)
+            mean_embedding, article_ids, db, page, page_size)
         return feed
