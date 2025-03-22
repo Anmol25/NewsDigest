70e9541037b4b2efe8bcf08762a18db3e8e8b764
Anmol Jindal <anmoljindal025@gmail.com>
2025-03-22 15:20:30 +0530

    Modified Appearance of Profile SideBar

--- apps/frontend/src/components/ProfileOptions/ProfileOptions.jsx
+++ apps/frontend/src/components/ProfileOptions/ProfileOptions.jsx
@@ -1,9 +1,26 @@
 import "./ProfileOptions.css"
+import { NavLink } from "react-router-dom";
+import history from "../../assets/history.svg"
+import user from "../../assets/user.svg"
+import exit from "../../assets/exit.svg"
 
 function ProfileOptions(){
     return(
         <div className="ProfileOptionsContainer">
-            <p>Profile Options</p>
+            <div className="ProfileOptionsList">
+                <NavLink to={"/profile/details"}>
+                    <img src={user} alt="User" />
+                    <span>Profile</span>
+                </NavLink>
+                <NavLink to={"/profile/history"}>
+                    <img src={history} alt="History" />
+                    <span>History</span>
+                </NavLink>
+                <button>
+                    <img src={exit} alt="Logout" />
+                    <span>Logout</span>
+                </button>
+            </div>
         </div>
     )
 }
