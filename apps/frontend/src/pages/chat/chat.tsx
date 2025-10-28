ff82acf2aabdd88a9f5ef4262d262acd83a103bc
Anmol Jindal <anmoljindal025@gmail.com>
2025-10-28 09:25:16 +0000

    Updated Navbar

--- apps/frontend/src/pages/chat/chat.tsx
+++ apps/frontend/src/pages/chat/chat.tsx
@@ -1,8 +1,9 @@
 function Chat(){
     return (
-        <div className="h-full w-full p-5">
-            <div className="w-full h-full border rounded-2xl border-gray-200  overflow-y-auto p-2.5 scrollbar-thin shadow-md">
-
+        <div className="h-full w-full p-5 pt-2.5 ">
+            <div className="flex w-full h-full border-2 rounded-2xl border-[#F0F0F0]  overflow-y-auto p-0 scrollbar-thin shadow-md bg-[#F0F0F0]">
+                <div className="w-[17.5%] rounded-2xl bg-white shadow-md"></div>
+                <div className="w-[82.5%]"></div>
             </div>
         </div>
     );
